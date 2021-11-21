785a5f831af6bf3b1774156563652475775a14f5
The OffBeat Doc™ <drkoustavsinharay@gmail.com>
2021-11-21 13:49:13 +0530

    added ui for easy usage from terminal

--- directoryList.py
+++ directoryList.py
@@ -40,6 +40,25 @@ def tree(dir_path: Path, level: int=-1, limit_to_directories: bool=False,
     print(f'\n{directories} directories' + (f', {files} files' if files else ''))
 
 
-tree(Path.home() / 'Documents', level=2, limit_to_directories=True, length_limit=100)
+#tree(Path.home() / 'Documents', level=2, limit_to_directories=True, length_limit=100)
 #you might not need to use all the parameters
 #for folders in 'C:\Users\Koustav Sinha Ray\' we do not need to specify the full path, pass 'Documents' as parameter for 'C:\Users\Koustav Sinha Ray\Document'
+
+def ui():
+    print("DIRECTORY LIST VIEW v1.0")
+    userPath = input("Enter path: ")
+    userBoolean1 = input("Do you want to continue and list path with default settings? (y/n):")
+    if (userBoolean1=="y"):
+        tree(Path.home() / userPath)
+    else:
+        userlimit_to_directories=False
+        userLevel = int(input("Restrict directory listing level to (e.g. 2, 3; default: -1, lists all subdirectories/files):"))
+        userBoolean2 = input("Limit listing to directory names? (y/n; default:n):")
+        if (userBoolean2=="y"):
+            userlimit_to_directories=True
+        
+        userLengthLimit = int(input("Enter length limit (default: 1000):"))
+        print("Displaying results as per user specified settings:")
+        tree(userPath, level=userLevel, limit_to_directories=userlimit_to_directories, length_limit=userLengthLimit)
+
+ui()
